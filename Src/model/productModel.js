97a470f574c5200b80ce589652c7c9132634cccf
gayathirithavamani <gayat2000@gmail.com>
2023-04-08 15:15:27 +0530

    PUSH

--- Src/model/productModel.js
+++ Src/model/productModel.js
@@ -1,34 +1,34 @@
 //Require
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Schema
-const Product = new mongoose.Schema({
-    
-    price :{
-        type: Number,
-        required: true,
-        collection: String
+const Product = new mongoose.Schema(
+  {
+    price: {
+      type: Number,
+      required: true,
+      collection: String,
     },
-    imgUrl :{
-        type: String,
-        required: true
-        
+    imgUrl: {
+      type: String,
+      required: true,
     },
-    title :{
-        type: String,
-        required: true,
+    title: {
+      type: String,
+      required: true,
     },
-    count:{
-        type: Number,
-        required : true,
-        default : 1
+    count: {
+      type: Number,
+      required: true,
+      default: 1,
     },
-    dis:{
-        type:String,
-        required:true
-    }
-
-},{timestamps : true})
+    dis: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 //Export;
-module.exports = mongoose.model('Product',Product,"Product_Data");
+module.exports = mongoose.model("Product", Product, "Product_Data");
